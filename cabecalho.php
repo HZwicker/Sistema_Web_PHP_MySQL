9b1eb40a63b1d900f7329cdc82cf3de6a0faee52
hzwicker <hickzwicker@gmail.com>
2019-05-27 19:55:02 -0300

    Initial Commit

---
+++ cabecalho.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Minha Loja Henrique</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/loja.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="principal">
---
+++ produto-formulario.php
@@ -0,0 +1,14 @@
+<?php include ("cabecalho.php")?>
+
+        <h1>Formulário de Produto</h1>
+<form action="adiciona-produto.php">
+    Nome:
+    <input type="text" name="nome"><br/><br/>
+    Preço:
+    <input type="number" name = "preco"><br/><br/>
+
+    <input type="submit" value="Cadastrar">
+
+</form>
+
+<?php include ("rodape.php") ?>
